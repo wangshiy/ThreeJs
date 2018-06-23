5c66ed764f4a415361800841aca7d3bcb22589e4
wangshiyu <wangshiyu.sw@bytedance.com>
2018-06-23 21:16:59 +0800

    [feature] init project with three.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+require('./a.js')
+require('./b.js')
+const a = 'a';
+var THREE = require('three');
+console.log(a, '我是index.js', THREE)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack')
+var path =require('path')
+module.exports = {
+  entry:{
+    app:path.join(__dirname,'src','index.js')
+  },
+  output:{
+    filename:'bundle.js',
+    path:path.join(__dirname,'dist')
+  },
+  plugins:[
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    inline:true, //HMR enable
+    hot:true, //HMR enable
+    compress: true, //Gzip enable
+    contentBase: path.join(__dirname, "dist"), //设置webpack-dev-server的根路径
+    port: 7000,
+    host: '127.0.0.1',
+    historyApiFallback:{
+      rewrites:[{
+        from:/./,
+        to:'/404.html'
+      }]
+    }, //404的重定向页面
+    overlay: true, //将server端的报错显示在client端
+    stats: "errors-only" //运行webpack-dev-server时只显示报错
+  }
+}
