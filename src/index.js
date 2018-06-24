0c78b7e6251d5e44512e5b6549a23ef4ccab3f2d
wangshiyu <wangshiyu.sw@bytedance.com>
2018-06-24 10:32:08 +0800

    [feature] 增加基本旋转立方体

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-require('./a.js')
+import basic from './basic.js';
 require('./b.js')
-const a = 'a';
-// var THREE = require('three');
 import * as THREE from 'three';
 import '@three-examples/controls/OrbitControls'; // import side-effects, so THREE can have OrbitControls method
-console.log(a, THREE)
+console.log(THREE)
